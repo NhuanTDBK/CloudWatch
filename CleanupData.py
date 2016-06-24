130f9c3630bd3ffdaa0f1b224b001e60e2597548
Dang Tran <dangtv18@gmail.com>
2016-06-24 11:47:39 +0700

    clean dataset

--- CleanupData.py
+++ CleanupData.py
@@ -4,14 +4,15 @@ import json
 import os
 
 print os.getcwd()
-folder_name = 'data'
+folder_name = 'data/dataset1'
 columns = ["points","timestamp"]
-for name in os.listdir('data'):
-        filename = "%s/%s"%(folder_name,name)
-        dat = json.load(open(filename,'rb'))[0]
-        df = pd.DataFrame(dat["datapoints"],columns=columns)
-        timeindex = pd.to_datetime(df.timestamp,unit='s')
-        df = df.set_index(timeindex)
-        df.to_csv('%s_remake'%filename)
+for name in os.listdir('data/dataset1'):
+        if (name.endswith(".json")):
+                filename = "%s/%s"%(folder_name,name)
+                dat = json.load(open(filename,'rb'))[0]
+                df = pd.DataFrame(dat["datapoints"],columns=columns)
+                timeindex = pd.to_datetime(df.timestamp,unit='s')
+                df = df.set_index(timeindex)
+                df.to_csv('%s_remake'%filename)
 
 
