fca4c1a36e6ef4a6ce2c5a7483010e6d9937c5e8
Dang Tran <dangtv18@gmail.com>
2016-06-25 16:09:54 +0700

    add custom_period for engine SHESD

--- experiments/TestEngine.py
+++ experiments/TestEngine.py
@@ -2,15 +2,17 @@ from service.BaseEngine import BaseEngine
 from utils.auto_load import AutoLoad
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from perioddetection import autoperiod
 loader = AutoLoad()
 engine = loader.auto_load_engine_default(method='SHESD')
-dat = pd.read_csv('../data/dataset1/2.json_remake',index_col=0,parse_dates=True)
+dat = pd.read_csv('../data/vc_1.json_remake',index_col=0,parse_dates=True)
 engine.longterm = True;
-engine.piecewise_median_period = 7;
+engine.piecewise_median_period = 4;
 engine.max_anoms = 1/200.0
 engine.fit(data=dat.points)
-print autoperiod.period_detect(engine.data,segment_method = "topdownsegment")
-# engine.fit_predict(data=dat.points[:-30])
-# engine.plot()
-# plt.show()
+engine.custom_period = np.min(autoperiod.period_detect(engine.data,segment_method = "slidingwindowsegment"))
+print engine.custom_period
+engine.fit_predict(data=dat.points)
+engine.plot()
+plt.show()
--- outlier/SHESD.py
+++ outlier/SHESD.py
@@ -17,7 +17,7 @@ class SHESD(BaseOutlier):
                  e_value=False, longterm=True,
                  piecewise_median_period=3, plot=False,
                  y_log=False, xlabel = '', ylabel = 'count',
-                 title=None, verbose=False):
+                 title=None, verbose=False, period = None):
         self.max_anoms = max_anoms
         self.direction = direction
         self.alpha = alpha
@@ -32,6 +32,7 @@ class SHESD(BaseOutlier):
         self.ylabel = ylabel
         self.title = title
         self.verbose = verbose
+        self.custom_period = period
     def convert_twitter_format(self,data=None):
         columns = ["timestamp", "points"]
         b = DataFrame(index=np.arange(data.shape[0]), columns=columns)
@@ -52,7 +53,7 @@ class SHESD(BaseOutlier):
         results = detect_ts(data, max_anoms=self.max_anoms,
                             direction=self.direction, alpha=self.alpha, only_last=self.only_last,
                             threshold=self.threshold, e_value=self.e_value, longterm = self.longterm,
-                            piecewise_median_period=self.piecewise_median_period)
+                            piecewise_median_period=self.piecewise_median_period, custom_period=self.custom_period)
         anoms = results['anoms']
         self.anomaly_idx = anoms.index
         self.anom_val = anoms['anoms']
